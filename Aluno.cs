306b6d389cba4a5ea486c3001d5a7775e42a3665
Camila Zelenovsky <camila_zele@hotmail.com>
2020-07-22 17:56:45 -0300

    First Commit

---
+++ Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classe
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public string Sala { get; set; }
+        public int Numero { get; set; }
+
+        public int[] notes = { 8, 9, 10, 5 };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            aluno.Nome = "Camila";
+            aluno.Sala = "3ª série";
+            aluno.Numero = 6;
+
+            Console.WriteLine("Qual é o bimestre que você deseja saber a nota?");
+            int bimestre = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"A nota da aluna {aluno.Nome}, número {aluno.Numero} da sala {aluno.Sala} no {bimestre}º bimestre é {aluno.notes[bimestre - 1]}.");
+
+            Console.WriteLine($"E sua média geral é {aluno.notes.Average()}.");
+        }
+    }
+}
